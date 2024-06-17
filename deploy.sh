587aea51734f868dd648adb44bf37b3cc430875c
Dan <dan@example.com>
2024-06-17 16:58:54 -0400

    add reader link deploy var

--- deploy.sh
+++ deploy.sh
@@ -54,6 +54,7 @@ if [[ "$DEPLOY_ENV" = "prod" ]]; then
   export GITEA__mailer__USER=$(op read "op://Shared-IT-Development/d52sfisg5cry5yfpj2lynfq3ru/username")
   export GITEA__mailer__FROM=$(op read "op://Shared-IT-Development/d52sfisg5cry5yfpj2lynfq3ru/username")
   export GITEA__mailer__PASSWD=$(op read "op://Shared-IT-Development/d52sfisg5cry5yfpj2lynfq3ru/password")
+  export READER_BASE_LINK=read.bibletranslationtools.org
 fi
 
 # Docker-compose vars
