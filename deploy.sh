a995bfb5f36156246c8e9bd133e61c4963e959a8
Dan <dan@example.com>
2024-06-18 11:06:52 -0400

    fix typo pt 3

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,7 @@ if [[ "$DEPLOY_ENV" = "prod" ]]; then
   export GITEA__mailer__USER=$(op read "op://Shared-IT-Development/d52sfisg5cry5yfpj2lynfq3ru/username")
   export GITEA__mailer__FROM=$(op read "op://Shared-IT-Development/d52sfisg5cry5yfpj2lynfq3ru/username")
   export GITEA__mailer__PASSWD=$(op read "op://Shared-IT-Development/d52sfisg5cry5yfpj2lynfq3ru/password")
-  export READER_BASE_LINK=https://read-dev.bibleineverylanguage.org/
+  export READER_BASE_LINK=https://read.bibleineverylanguage.org/
 fi
 
 # Docker-compose vars
